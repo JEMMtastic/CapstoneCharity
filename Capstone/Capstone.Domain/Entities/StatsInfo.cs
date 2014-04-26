a6651d3fc2d9452036fabab9bac5a5e68567cc0b
mpolicicchio <aliquisme@gmail.com>
2014-04-25 23:01:55 -0700

    Added StatsInfo Class
    Added the properties of the class.

---
+++ Capstone/Capstone.Domain/Entities/StatsInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Domain.Entities
+{
+    class StatsInfo
+    {
+        public int StatsInfoId { get; set; }
+        public decimal TotalSales { get; set; }
+        public decimal AmountOfTotalSalesToCharity { get; set; }
+        public decimal CashDonations { get; set; }
+        public int GuestCount { get; set; }
+    }
+}

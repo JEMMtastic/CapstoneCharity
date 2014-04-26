f553be808458a55b5ada4884d36f8e506a1895ea
mpolicicchio <aliquisme@gmail.com>
2014-04-25 23:44:14 -0700

    Populated StatsInfo Interface and Repository
    Threw in some generic CRUD methods.

--- Capstone/Capstone.Domain/Abstract/StatsInfoInterface.cs
+++ Capstone/Capstone.Domain/Abstract/StatsInfoInterface.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Domain.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Capstone.Domain.Abstract
 {
     interface StatsInfoInterface
     {
+        void AddStatsInfo(StatsInfo s);
+        User GetStatsInfo(int id);
+        void DeleteStatsInfo(int id);
+        void EditStatsInfo(int id);
     }
 }
--- Capstone/Capstone.Domain/Concrete/StatsInfoRepository.cs
+++ Capstone/Capstone.Domain/Concrete/StatsInfoRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capstone.Domain.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,26 @@ using System.Threading.Tasks;
 
 namespace Capstone.Domain.Concrete
 {
-    class StatsInfoRepository
+    class StatsInfoRepository : StatsInfoInterface
     {
+        public void AddStatsInfo(Entities.StatsInfo s)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entities.User GetStatsInfo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteStatsInfo(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditStatsInfo(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

63eb2343481fb573c40c668f4e6042c53e59caff
Jessica M <laughingllamalama@gmail.com>
2014-04-25 23:57:31 -0700

    Added PartnershipNight Class
    Made everything auto get/set.  can change later if necessary

---
+++ Capstone/Capstone.Domain/Entities/PartnershipNight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Capstone.Domain.Entities
+{
+    class PartnershipNight
+    {
+        public int PartnershipNightId { get; set; }
+        public DateTime Date { get; set; }
+        public Charity Charity { get; set; }
+        public BvLocation BVLocation { get; set; }
+        public int CheckRequestId { get; set; }
+        public decimal AmountRaised { get; set; }
+        public string Comments { get; set; }
+        public bool CheckRequestFinished { get; set; }
+        public bool BeforeTheEventFinished { get; set; }
+        public bool AfterTheEventFinished { get; set; }
+    }
+}

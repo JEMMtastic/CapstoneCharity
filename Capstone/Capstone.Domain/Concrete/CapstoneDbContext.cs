e6b47cccb13496beb47469341a782bf3ac1ecff0
Jessica M <laughingllamalama@gmail.com>
2014-04-26 19:36:37 -0700

    Added EF 6,  DbContext and connection string
    CapstoneDbContext and connection string

---
+++ Capstone/Capstone.Domain/Concrete/CapstoneDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Capstone.Domain.Entities;
+using System.Data.Entity;
+
+namespace Capstone.Domain.Concrete
+{
+    class CapstoneDbContext
+    {
+        DbSet<BvLocation> BvLocations { get; set; }
+        DbSet<User> Users { get; set; }
+        DbSet<Charity> Charities { get; set; }
+        DbSet<PartnershipNight> PartnershipNights { get; set; }
+        DbSet<StatsInfo> StatsInfos { get; set; }
+    }
+}
